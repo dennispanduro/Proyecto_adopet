bdc954a891a5a45c9cf1c708945af76d76c2922d
Dennis30 <72525447@certus.edu.pe>
2025-06-25 23:03:08 -0500

    Avance12

---
+++ DB/data.js
@@ -0,0 +1,56 @@
+const productos = [
+  {
+    nombre: 'Polera Blue Star',
+    categoria: 'ropa',
+    imagen: '../Producto-mascotas/perrito (2).png',
+    botonTexto: 'COMPRAR'
+  },
+  {
+    nombre: 'Chaleco contra la ansiedad',
+    categoria: 'juguetes',
+    imagen: '../Producto-mascotas/perrito (3).png',
+    botonTexto: 'COMPRAR'
+  },
+  {
+    nombre: 'Vestido Reuna Tulipán',
+    categoria: 'ropa',
+    imagen: '../Producto-mascotas/perrito (4).png',
+    botonTexto: 'COMPRAR'
+  },
+  {
+    nombre: 'Camisa Beige Sunser',
+    categoria: 'camas',
+    imagen: '../Producto-mascotas/perrito (5).png',
+    botonTexto: 'COMPRAR'
+  },
+  {
+    nombre: 'Camisa Sugar Melón',
+    categoria: 'ropa',
+    imagen: '../Producto-mascotas/perrito (6).png',
+    botonTexto: 'COMPRAR'
+  },
+  {
+    nombre: 'Camisa Red Poppy',
+    categoria: 'comida',
+    imagen: '../Producto-mascotas/perrito (7).png',
+    botonTexto: 'COMPRAR'
+  },
+  {
+    nombre: 'Poleta dog Pride',
+    categoria: 'ropa',
+    imagen: '../Producto-mascotas/perrito (8).png',
+    botonTexto: 'COMPRAR'
+  },
+  {
+    nombre: 'Love is love',
+    categoria: 'juguetes',
+    imagen: '../Producto-mascotas/perrito (9).png',
+    botonTexto: 'COMPRAR'
+  },
+  {
+    nombre: 'Polera Pride Day',
+    categoria: 'ropa',
+    imagen: '../Producto-mascotas/perrito (1).png',
+    botonTexto: 'COMPRAR'
+  }
+];
---
+++ reporte.md
@@ -0,0 +1,29 @@
+# Sección 1: Tipografías
+("• Montserrat")
+("• Intro Black")
+("• Intro Regular")
+("• Inter")
+
+# Sección 2: Font-weight
+("• 300 (Light)")
+("• 500 (Medium)")
+("• 700 (Bold)")
+("• 800 (Extra Bold)")
+
+# Sección 3: Tamaños de letra principales
+("• 14px")
+("• 18px")
+("• 24px")
+("• 28px")
+("• 40px")
+("• 64px")
+("• 96px")
+
+# Sección 4: Colores principales usados
+
+colores_principales 
+    hsl(36, 61%, 69%) ,  # Amarillo principal
+    hsl(129, 40%, 37%) ,  # Texto principal oscuro
+    hsl(129, 18%, 80%) ,  # Verde destacado
+    hsl(0, 0%, 21%) ,  # Fondo tarjetas
+    hsl(36, 90%, 20%) ,  # Footer
