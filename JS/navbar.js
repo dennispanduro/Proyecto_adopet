40b9820d09ee94e6548de2fff919f0f50e1d7352
Dennis30 <72525447@certus.edu.pe>
2025-06-25 23:17:22 -0500

    Avance5

--- JS/navbar.js
+++ JS/navbar.js
@@ -5,7 +5,7 @@ const navbarHTML = `
   <label for="menu-toggle" class="hamburguesa">&#9776;</label>
   <div class="nav">
     <ul>
-      <li><a class="boton-navegacion" href="../index.html">INICIO</a></li>
+      <li><a class="boton-navegacion" href="../HTML/index.html">INICIO</a></li>
       <li><a class="boton-navegacion" href="../HTML/tienda.html">TIENDA</a></li>
       <li><a class="boton-navegacion" href="">SERVICIOS</a></li>
       <li><a class="boton-navegacion" href="">ADOPCIONES</a></li>
